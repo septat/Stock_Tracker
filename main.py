0756a388a4246818c68b45e828780cf3aa0bf6b7
septat <evanluu2614@gmail.com>
2021-01-24 01:23:02 -0800

    small incremental change

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 import yfinance as yf
+from scipy import stats 
+import requests
 
 st.title('Stock Tracker')
 
@@ -11,3 +13,7 @@ st.markdown("""
 This project aims to present any ***given stock*** currently existing on the NYSE with information regarding opening and closing price, its price to expense ratio, and a graph charting its performance.
 * **Python libraries:** pandas, streamlit, numpy, matplotlib, yfinance    
 """)
+
+st.sidebar.header('User Input Features')
+
+@st.cache
