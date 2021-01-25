7aaf162ee1b53880f94d4574442fa9c0f6ed5e8e
septat <evanluu2614@gmail.com>
2021-01-24 20:52:44 -0800

    Added stock input and data gathering

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import seaborn as sns
 import numpy as np
 import yfinance as yf
 from scipy import stats 
-import requests
+#import requests
+import base64
 
 st.title('Stock Tracker')
 
@@ -14,6 +15,32 @@ This project aims to present any ***given stock*** currently existing on the NYS
 * **Python libraries:** pandas, streamlit, numpy, matplotlib, yfinance    
 """)
 
-st.sidebar.header('User Input Features')
 
-@st.cache
+stock_symbol = st.text_input("Please Insert Valid Stock Symbol Below")
+
+
+data = yf.download(
+        tickers = stock_symbol,
+        period = "ytd",
+        interval = "1d",
+        group_by = 'ticker',
+        auto_adjust = True,
+        prepost = True,
+        threads = True,
+        proxy = None
+    )
+
+print(data)
+
+### THE SIDEBAR CANT BE IMPLEMENTED FOR LACK OF CONCRETE IDEA OF IMPLEMENTATION
+#st.sidebar.header('User Input Features')
+
+
+
+# CSV FILE DOWNLOAD # 
+# def filedownload(df):
+#     csv = df.to_csv(index=False)
+#     # strings <-> bytes conversions
+#     b64 = base64.b64encode(csv.encode()).decode()
+#     href = f'<a href="data:file/csv;base64,{b64}" download="SP500.csv">Download CSV File</a>'
+#     return href
